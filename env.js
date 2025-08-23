d29026e377936e1736d53f2527dcd2d03fb0e2fc
oacoc3 <oacoc3@gmail.com>
2025-08-23 09:25:15 -0300

    Update env.js

--- env.js
+++ env.js
@@ -1,6 +1,5 @@
-<script>
-  window.ENV = {
-    SUPABASE_URL: "https://egoglbcvelzokuivcyfc.supabase.co",
-    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVnb2dsYmN2ZWx6b2t1aXZjeWZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU5NDg5ODAsImV4cCI6MjA3MTUyNDk4MH0.QRV220TmW-l-LBFQWCpr18CEhrenr-lcSUnMnZXflRg"
-  };
-</script>
+// env.js (apenas JS, sem <script> e </script>)
+window.ENV = {
+  SUPABASE_URL: "https://egoglbcvelzokuivcyfc.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVnb2dsYmN2ZWx6b2t1aXZjeWZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU5NDg5ODAsImV4cCI6MjA3MTUyNDk4MH0.QRV220TmW-l-LBFQWCpr18CEhrenr-lcSUnMnZXflRg"
+};
