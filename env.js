4f30f2616e567b86b5903cad06db404bf65d63d1
oacoc3 <oacoc3@gmail.com>
2025-08-24 20:28:53 -0300

    Update env.js

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
 // env.js (apenas JS, sem <script> e </script>)
 window.ENV = {
-  SUPABASE_URL: "https://tnfrabiafdubcqyxahnx.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRuZnJhYmlhZmR1YmNxeXhhaG54Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYwNzY0ODgsImV4cCI6MjA3MTY1MjQ4OH0.zEEX8zLdYMT88cXS1dSHdgxzTaEnueceg4uy4gBnjH4"
+  SUPABASE_URL: "https://sgvdczdjuvcrfjosshfi.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNndmRjemRqdXZjcmZqb3NzaGZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYwNzc0MjYsImV4cCI6MjA3MTY1MzQyNn0.ujHz8hirZqqrdTiaKmlmHI5fwOSEPIdBuTQDI60tfAY"
 };
